732797640cdf138fc76c66190a0c23c8e5d51b5b
Erickson Zhuang <bingluen0224@gmail.com>
2017-10-15 01:46:42 +0800

    Output as json

--- convert_to_json.js
+++ convert_to_json.js
@@ -2,8 +2,6 @@ var fs = require('fs-extra');
 var rp = require('request-promise');
 
 let inputFilename = process.argv[2]
-let outputFilename = process.argv[3] || 'output.json'
-
 let input = fs.readFileSync(inputFilename, 'utf8')
 
 let lines = input.split(/\n/).map((line) => {
@@ -17,36 +15,44 @@ let lines = input.split(/\n/).map((line) => {
     }
 })
 
-function promiseGetPOS(raw) {
-    return rp({
-        method: 'POST',
-        uri: 'http://localhost:8999',
-        body: raw.sentences
-    }).then((res) => {
-        return {
-            raw: raw,
-            result: JSON.parse(res)
-        };
-    });
-}
+fs.writeJSONSync('move_lines.json', lines)
 
-var lineChunks = []
+// function promiseGetPOS(raw) {
+//     return rp({
+//         method: 'POST',
+//         uri: 'http://localhost:8999',
+//         body: raw.sentences
+//     }).then((res) => {
+//         return {
+//             raw: raw,
+//             result: JSON.parse(res)
+//         };
+//     });
+// }
 
-while(lines.length) {
-    lineChunks.push(lines.splice(0,1000))
-}
+// var lineChunks = []
 
-lineChunks.forEach((lineChunk, index) => {
-    let tasks = lineChunk.map((element) => promiseGetPOS(element));
+// while(lines.length) {
+//     lineChunks.push(lines.splice(0,1000))
+// }
 
-    Promise.all(tasks)
-        .then((result) => {
-            console.log('part' + index + ' - ' + (index * 1000) + '~' + ((index + 1) * 1000 - 1))
-            fs.writeJsonSync('part' + index + '_' + outputFilename, result)
-            console.log(result)
-        })
-        .catch((err) => {
-            console.log('part' + index + ' - ' + (index * 1000) + '~' + ((index + 1) * 1000 - 1))
-            console.error(err)
-        })
-})
+// let chunkCount = 0
+
+// function genTasksPromise(lineChunk) {
+//     return lineChunk.map((element) => promiseGetPOS(element))
+// }
+
+// lineChunks.forEach((lineChunk, index) => {
+//     let tasks = lineChunk.map((element) => promiseGetPOS(element));
+
+//     Promise.all(tasks)
+//         .then((result) => {
+//             console.log('part' + index + ' - ' + (index * 500) + '~' + ((index + 1) * 1000 - 1))
+//             fs.writeJsonSync('part' + index + '_' + outputFilename, result)
+//             console.log(result)
+//         })
+//         .catch((err) => {
+//             console.log('part' + index + ' - ' + (index * 500) + '~' + ((index + 1) * 1000 - 1))
+//             console.error(err)
+//         })
+// })
