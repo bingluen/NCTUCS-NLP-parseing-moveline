dbba3118c36927f7b9d6fb56a39bdc4afe791857
Erickson Zhuang <bingluen0224@gmail.com>
2017-10-23 07:57:23 +0800

    Refactoring

--- task1.js
+++ task1.js
@@ -1,71 +1,91 @@
 var fs = require('fs-extra');
-var _ = require('lodash/core');
-
-let word_count_per_line = []
-let word_count_global = []
 
 files = fs.readdirSync('./').filter(file => file.match('output'))
 
-files.forEach((el) => {
-    rawJSON = JSON.parse(fs.readFileSync(el))
-    rawJSON.splice(9, 20).filter((el) => el['sentences'].length > 0).forEach((el) => {
-        word_count_per_line.push({
-            lineId: el['lineId'],
-            sentences: el['sentences'],
-            tokens: el['parsing']['sentences'][0]['tokens'].map((el) => ({
-                lemma: el['lemma'],
-                pos: el['pos'],
-                count: 1
-            })).reduce((pv, cv, index, array) => {
-                sameWord = pv.find((el) => el.lemma === cv.lemma)
-                if (sameWord === undefined) {
-                    pv.push(cv)
-                } else {
-                    sameWord.count += 1
-                }
-                return pv
-            }, [])
-        })
-    })
+var idf_table = []
+var tf_idf_table = []
 
-    word_count_global = word_count_per_line.map((el) => el.tokens)
-        .reduce((pv, cv) => pv.concat(cv), [])
-        .reduce((pv, cv, index, array) => {
-            sameWord = pv.find((el) => el.lemma === cv.lemma)
-            if (sameWord === undefined) {
-                pv.push({
-                    lemma: cv.lemma,
-                    pos: cv.pos,
-                    count: 1
-                })
-            } else {
-                sameWord.count +=1
-            }
-            return pv
-        }, [])
-});
+files.forEach(el => {
+	rawJSON = JSON.parse(fs.readFileSync(el))
+		// Remove empty sentences
+		.filter(el => el['sentences'].length > 0)
 
+	// Alias
+	let lines = rawJSON
 
-// Compute idf
-word_count_global.forEach((el) => {
-    el['idf'] = Math.log10(1.0 * word_count_per_line.length / el.count)
+	lines.forEach(line => {
+		let tokenCollection = line.parsing.sentences
+			.map(sentences => sentences.tokens)
+			.reduce((collection, tokens) => collection.concat(tokens), [])
+		// Count tf for each line
+		tf_idf_table.push({
+			lineId: line.lineId,
+			sentences: line.sentences,
+			tokens: tokenCollection
+				// remove duplicate token
+				.filter((token, index, tokens) =>
+					tokens.slice(0, index).findIndex(previousToken => 
+						previousToken.lemma === token.lemma) === -1
+					)
+				// mapping to tf for each unique token
+				.map(uniqueToken => ({
+					lemma: uniqueToken.lemma,
+					pos: uniqueToken.pos,
+					ref: tokenCollection
+						// find same toke in same line
+						.filter(token => token.lemma === uniqueToken.lemma)
+						// get those index in line
+						.map(token => token.index),
+					tf: tokenCollection
+						// find same toke in same line
+						.filter(token => token.lemma === uniqueToken.lemma)
+						// get those index in line
+						.map(token => token.index).length
+				}))
+		})
+
+		// Count reference of each token
+		tokenCollection
+			// remove duplicate token
+			.filter((token, index, tokens) =>
+				tokens.slice(0, index).findIndex(previousToken => 
+					previousToken.lemma === token.lemma) === -1
+				)
+			.forEach(uniqueToken => {
+				// find them on idf table
+				target = idf_table.find(token => token.lemma === uniqueToken.lemma)
+				if (target === undefined) {
+					// If not exist, add it.
+					idf_table.push({
+						lemma: uniqueToken.lemma,
+						pos: uniqueToken.pos,
+						ref: [line.lineId],
+					})
+				} else {
+					// else update it.
+					target.ref.push(line.lineId)
+				}
+			})
+
+			// Alias
+			let numLines = tf_idf_table.length
+
+			// Compute idf for each token
+			idf_table.forEach(token => {
+				token.idf = Math.log10(1.0 * numLines / token.ref.length)
+			})
+
+	})
 })
 
-// Compute tf-idf
-word_count_per_line.forEach((line) => {
-    line.tokens.forEach((token) => {
-        idf = word_count_global.find((word) => word.lemma === token.lemma).idf
-        token['tf-idf'] = 1.0 * token.count * idf
-    })
+// Compute tf-idf for each token on each lin
+tf_idf_table.forEach(line => {
+	line.tokens.forEach(token => {
+		let idf = idf_table.find(idfToken => idfToken.lemma === token.lemma).idf
+		token['tf-idf'] = token.tf * idf
+	})
 })
 
-// console.log('word count per line')
-// console.log(word_count_per_line)
-// word_count_per_line.forEach((el) => { console.log(el) })
-// console.log('word count global')
-// console.log(word_count_global)
-
-// Select 300 word
 /**
  * ＰＯＳ黑名單
  *  助動詞、BE動詞 VBP
@@ -82,64 +102,57 @@ word_count_per_line.forEach((line) => {
  */
 var removePOS = ['VBP', 'DT', 'CC', 'IN', 'PRP', '!', ',', '.', ':', '\'\'', 'WP', 'WRB', 'WDT', '``', 'TO']
 
-// 統計每個 word 的 tf-idf，如果多句出現則以數值高的為基準，最後把 idf 合併回去
-tf_idf_table = word_count_per_line
-    .map((el) => el.tokens)
-    .reduce((pv, cv, i) => {
-        cv.forEach((token) => {
-            let target = pv.find((el) => el.lemma === token.lemma);
-            if (target === undefined) {
-                pv.push(_.clone(token))
-            } else if (target['tf-idf'] < token['tf-idf']) {
-                target['tf-idf'] = token['tf-idf'];
-            }
-        })
-        return pv
-    }, [])
-    .map((token) => {
-        token.idf = word_count_global.find((el) => el.lemma === token.lemma).idf
-        return token
-    })
+let selectedTokens = tf_idf_table
+	// Get tokens
+	.reduce((tokens, line) => tokens.concat(line.tokens), [])
+	// Descending order by tf-idf 
+	.sort((a, b) => b['tf-idf'] - a['tf-idf'])
+	// Remove duplicate token
+	.filter((token, index, tokens) => 
+		tokens.slice(0, index).findIndex(previousToken => 
+			token.lemma === previousToken.lemma) === -1
+	)
+	// Remove non-semantic token by POS
+	.filter(token => removePOS.findIndex(pos => token.pos === pos) === -1)
+	// Mapping to lemma
+	.map(uniqueToken => uniqueToken.lemma)
+	// Get the first three hundred token
+	.slice(0, 300)
 
+// Output result
 
-//挑 tf-idf 高的
-var selectedWord = tf_idf_table.filter((el) => {
-    // remove 代名詞、文法動詞（be 動詞、助動詞）、介系詞、冠詞
-    return removePOS.findIndex((rm) => el.pos === rm) === -1
-}).sort((a, b) =>  {
-    return a['tf-idf'] <= b['tf-idf']
-}).splice(0, 300)
-
-
-// Output
-var selectedLine = ['L36326',
-    'L294442',
-    'L569822',
-    'L133500',
-    'L191930',
-    'L402729',
-    'L481523',
-    'L552446',
-    'L553640',
-    'L603014',]
-var selectedLemma = selectedWord.map((el) => el.lemma)
-var line1 = selectedLemma.map((el) => el.toLowerCase()).join('|')
-var line2 = selectedWord.map((el) => el.idf.toFixed(1)).join(',')
-var perLine = word_count_per_line.filter((line) => selectedLine.findIndex((lineId) => lineId === line.lineId) > -1).map(
-    (movie_line) => selectedLemma.map((word) => {
-        let target = movie_line.tokens.find((token) => token.lemma === word)
-        return target === undefined ? 0 : target['tf-idf'].toFixed(1)
-    }).join(',')
+let outputLines = []
+// line 1
+outputLines.push(
+	selectedTokens.map(token => token.toLowerCase()).join('|')
+)
+// line 2
+outputLines.push(
+	selectedTokens.map(selectedToken => 
+		idf_table.find(token => token.lemma === selectedToken).idf.toFixed(2)
+	).join(',')
 )
 
-var output = fs.createWriteStream('task1', {
-    flags: 'w',
-    encoding: 'utf8'
-})
+let selectedLines = ['L36326','L294442','L569822','L133500','L191930','L402729','L481523','L552446','L553640','L603014']
+// tf-idf table
+outputLines = outputLines.concat(
+	selectedLines
+		// Find selected line
+		.map(lineId => tf_idf_table.find(line => line.lineId === lineId))
+		// mapping to tf-idf data
+		.map(line => line.tokens
+			// Remove unselected tokens
+			.filter(token => 
+				selectedTokens.findIndex(selectedToken => token.lemma === selectedToken) > -1
+				)
+			.reduce((row, token) => {
+				let column = selectedTokens.findIndex(selectedToken => token.lemma === selectedToken)
+				row[column] = token['tf-idf'].toFixed(2)
+				return row
+			}, Array(selectedTokens.length).fill(0))
+			.join(',')
+		)
+)
 
-output.write(line1 + '\n')
-output.write(line2 + '\n')
-perLine.forEach((line) => {
-    output.write(line + '\n')
-})
-
+console.log(outputLines)
+fs.writeFileSync('task1', outputLines.join('\n'))
